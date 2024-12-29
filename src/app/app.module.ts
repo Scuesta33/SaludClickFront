07a09b482e29dc70459d9f31fd4a00f1dbfb5e5a
Sergio Cuesta <scuesta33@gmail.com>
2024-12-29 01:46:48 +0100

    Agregar componentes login y signup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { NavbarComponent } from './components/navbar/navbar.component'; 
+import { SignupComponent } from './pages/signup/signup.component'; // Corrected path
+
 @NgModule({
   declarations: [
-    AppComponent,
-    
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
-    NavbarComponent
+    NavbarComponent, // Import standalone component
+    SignupComponent // Import standalone component
   ],
   providers: [
     provideClientHydration(withEventReplay()),
     provideAnimationsAsync()
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA], // Add this line
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
-  standalone: false,
+  standalone: true,
   
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
 
