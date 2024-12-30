3c57aae1e87737e6b33bfe51c373176db38735c6
Sergio Cuesta Barrera <scuesta33@gmail.com>
2024-12-30 12:58:50 +0100

    Creando el home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,19 +5,19 @@ import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
 const routes: Routes = [
   {
-    path: '',
-    component: HomeComponent,
-    pathMatch: 'full'
+    path : '',
+    component : HomeComponent,
+    pathMatch : 'full'
   },
   {
-    path: 'signup',
-    component: SignupComponent,
-    pathMatch: 'full'
+    path : 'signup',
+    component : SignupComponent,
+    pathMatch : 'full'
   },
   {
-    path: 'login',
-    component: LoginComponent,
-    pathMatch: 'full'
+    path : 'login',
+    component : LoginComponent,
+    pathMatch : 'full'
   }
 
 ];
